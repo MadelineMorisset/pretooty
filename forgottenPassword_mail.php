728f4702bf9a8bc9d965db6a9a621773395396ed
Elodie Angenard <ange.elodie@gmail.com>
2023-06-07 11:02:34 +0200

    fonction mail réinitialisation mdp

--- forgottenPassword_mail.php
+++ forgottenPassword_mail.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pretooty, réinitialisation du mot de passe</title>
+        <title>Pretooty, demande de réinitialisation du mot de passe</title>
     </head>
 
     <body>
---
+++ forgottenPassword_resetPassword.php
@@ -0,0 +1,14 @@
+ <?php session_start(); ?>
+
+ <!DOCTYPE html>
+ <html lang="fr">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pretooty, Réinitialisation du mot de passe</title>
+ </head>
+ <body>
+    
+ </body>
+ </html>
