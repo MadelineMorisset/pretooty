3c46be3ca622a0ed2d515715b4a329856d9bb4ec
Elodie Angenard <ange.elodie@gmail.com>
2023-06-08 10:38:33 +0200

    récupération de l'id user pour le mail init password

--- mailInitPassword.php
+++ mailInitPassword.php
@@ -4,23 +4,24 @@ include('dataBaseConnexion.php');
 
 
 
-
 if (isset($_POST['mail']) || !empty($_POST['mail'])) {
+    $uniq_id = random_bytes(10);
 
     $subject = 'Mot de passe oublié';
-    $message = "Bonjour, voici le lien pour la ré-initialisation de votre mot de passe: \n C:/xampp/htdocs/git/pretooty/forgottenPassword_resetPassword.php";           
+    $message = "Bonjour, voici le lien pour la ré-initialisation de votre mot de passe: \n C:/xampp/htdocs/git/pretooty/forgottenPassword_resetPassword.php?id_user=$uniq_id";           
     $headers = 'From: Pretooty \n Content-Type: text/plain; charset="UTF-8"';
 
     if (mail($_POST['mail'], $subject, $message, $headers) == true) {
-        $query = $db->prepare("UPDATE utilisateur SET mdp = NULL WHERE mail = :mail");
+        $query = $db->prepare('UPDATE utilisateur SET mdp = NULL, id_provisoire = :provisoire WHERE mail = :mail');
         $query->execute([
-                "mail" => $_POST['email']
+                "mail" => $_POST['mail'],
+                "provisoire" => $uniq_id,
                 ]);
         echo "E-mail envoyé";
-        } 
-        else { 
+    } 
+    else { 
         echo "Une erreur est survenue";
-        }
+    }
 }
 
 
--- setNewPassword.php
+++ setNewPassword.php
@@ -7,12 +7,13 @@ include('dataBaseConnexion.php');
 if (isset($_POST['mdp']) || !empty($_POST['mdp']) || isset($_POST['confirm']) || !empty($_POST['confirm'])) {
     if ($_POST['mdp'] === $_POST['confirm']) {
 
+        $id_user = $_GET['id_user'];
         $mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
 
-        $query = $db->prepare('UPDATE utilisateur SET mdp = :mdp WHERE id_utilisateur = :id_user');
+        $query = $db->prepare('UPDATE utilisateur SET mdp = :mdp WHERE id_provisoire = :id_user');
         $query->execute([
                     "mdp" => $mdp,
-                    "id_user" => $id_user,                  // OU RECUPERER ID_USER ??????????
+                    "id_user" => $id_user,
                 ]);
     }
 } 
