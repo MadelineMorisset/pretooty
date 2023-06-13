682eecc495c74226999947954bcaff5e9a939d3d
Elodie Angenard <ange.elodie@gmail.com>
2023-06-13 11:24:01 +0200

    fonctions onglet parametres

--- functions.php
+++ functions.php
@@ -3,59 +3,94 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
 // USERPAGEACCOUNT
 
-function showMyAccountGeneral() {
-    include('dataBaseConnexion.php');
-    $query = $db->prepare('SELECT * FROM utilisateur INNER JOIN categorie_u ON utilisateur.categorie_u = categorie_u.id_categorie_u
-WHERE utilisateur.id_utilisateur = :id_utilisateur');
-    $query->execute([
-        "id_utilisateur" => $_SESSION['id_utilisateur'],
+    // ONGLET GENERAL
+
+    function showMyAccountGeneral() {
+        include('dataBaseConnexion.php');
+        $query = $db->prepare('SELECT * FROM utilisateur INNER JOIN categorie_u ON utilisateur.categorie_u = categorie_u.id_categorie_u
+    WHERE utilisateur.id_utilisateur = :id_utilisateur');
+        $query->execute([
+            "id_utilisateur" => $_SESSION['id_utilisateur'],
+            ]);
+        $users = $query->fetchAll();
+
+        foreach($users as $user) {
+            echo '<div>Catégorie : '.$user['nom_categorie_u'].' </div>
+            <div>Pseudonyme : '.$user['pseudonyme'].' </div>
+            <div>Nom : '.$user['nom_utilisateur'].' </div>
+            <div>Prénom : '.$user['prenom_utilisateur'].' </div>';
+        }
+    }
+
+    // ONGLET COORDONNEES
+
+    function showMyAccountContactDetails() {
+        include('dataBaseConnexion.php');
+        $query = $db->prepare('SELECT * FROM utilisateur WHERE id_utilisateur = :id_utilisateur');
+        $query->execute([
+            "id_utilisateur" => $_SESSION['id_utilisateur'],
         ]);
-    $users = $query->fetchAll();
+        $users = $query->fetchAll();
 
-    foreach($users as $user) {
-        echo '<div>Catégorie : '.$user['nom_categorie_u'].' </div>
-        <div>Pseudonyme : '.$user['pseudonyme'].' </div>
-        <div>Nom : '.$user['nom_utilisateur'].' </div>
-        <div>Prénom : '.$user['prenom_utilisateur'].' </div>';
+        foreach($users as $user) {
+            echo '<div>Adresse : '.$user['adresse'].' </div>
+            <div>Ville : '.$user['ville'].' </div>
+            <div>Code Postal : '.$user['cp'].' </div>
+            <div>Téléphone : '.$user['tel'].' </div>
+            <div>Adresse mail : '.$user['mail'].' </div>
+            <div>Mot de passe</div>';
+        }
     }
-}
 
+    
+    // ONGLET PARAMETRES
 
-function showMyAccountCoordonnees() {
-    include('dataBaseConnexion.php');
-    $query = $db->prepare('SELECT * FROM utilisateur WHERE id_utilisateur = :id_utilisateur');
-    $query->execute([
-        "id_utilisateur" => $_SESSION['id_utilisateur'],
-    ]);
-    $users = $query->fetchAll();
-
-    foreach($users as $user) {
-        echo '<div>Adresse : '.$user['adresse'].' </div>
-        <div>Ville : '.$user['ville'].' </div>
-        <div>Code Postal : '.$user['cp'].' </div>
-        <div>Téléphone : '.$user['tel'].' </div>
-        <div>Adresse mail : '.$user['mail'].' </div>
-        <div>Mot de passe</div>';
-    }
-}
+        // NOTIFICATIONS
 
 
 
+        // SUPPRIMER MON COMPTE
+
+        function deleteMyAccount() {
+            include('dataBaseConnexion.php');
+            $query = $db->prepare('DELETE FROM utilisateur WHERE id_utilisateur = :id_utilisateur');
+            $query->execute([
+                "id_utilisateur" => $_SESSION['id_utilisateur'],
+            ]);
+            $query->fetchAll();
+
+            echo '<div>Compte supprimé avec succès.</div>';    
+        }
+
+
+        // TAILLE DE LA POLICE ET THEME COULEUR
+
+        function chooseFontSize() {
+            include('dataBaseConnexion.php');
+            $query = $db->prepare('UPDATE utilisateur SET id_police = :id_police WHERE id_utilisateur = :id_utilisateur');
+            $query->execute([
+                "id_police" => $_POST['id_police'],
+                "id_utilisateur" => $_SESSION['id_utilisateur'],
+            ]);
+            $query->fetchAll();
+        }
+
+
+        function chooseThemeColor() {
+            include('dataBaseConnexion.php');
+            $query = $db->prepare('UPDATE utilisateur SET id_theme = :id_theme WHERE id_utilisateur = :id_utilisateur');
+            $query->execute([
+                "id_theme" => $_POST['id_theme'],
+                "id_utilisateur" => $_SESSION['id_utilisateur'],
+            ]);
+            $query->fetchAll();
+        }
 
 
 
 
 
-// $query = $db->prepare('SELECT * FROM utilisateur INNER JOIN theme ON utilisateur.id_theme = theme.id_theme
-//                                                     INNER JOIN outil ON utilisateur.id_utilisateur = outil.id_utilisateur
-//                                                     INNER JOIN categorie ON outil.id_categorie = categorie.id_categorie
-//                                                     INNER JOIN categorie_u ON utilisateur.categorie_u = categorie_u.id_categorie_u
-//                                                     INNER JOIN emprunt ON outil.id_outil = emprunt.id_outil
-//                                                     WHERE utilisateur.id_utilisateur = :id_utilisateur');
-//     $query->execute([
-//         "id_utilisateur" => $_SESSION['id_utilisateur'],
-//         ]);
-//     $users = $query->fetchAll();
 ?>
--- userPageAccount.php
+++ userPageAccount.php
@@ -14,7 +14,25 @@
 
         <?php include('functions.php');
         showMyAccountGeneral(); 
-        showMyAccountCoordonnees()?>
+        showMyAccountContactDetails()?>
+
+        <div>
+        <label for="id_police">Taille de la police</label>
+            <select name="id_police">
+                <option value="1">Petite</option>
+                <option value="2">Moyenne</option>
+                <option value="3">Grande</option>
+                <option value="4">Très grande</option>
+            </select>
+        </div>
+
+        <div>
+        <label for="id_theme">Thème couleur</label>
+            <select name="id_theme">
+                <option value="1">Pretooty</option>
+                <option value="2">Nuit</option>
+            </select>
+        </div>
 
 
         <?php include('footer.php'); ?>
