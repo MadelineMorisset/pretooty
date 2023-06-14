c34685d6e1f9dd4056bceeff4b8639c907fb2384
Madeline Morisset <12475097+MadelineMorisset@users.noreply.github.com>
2023-06-14 14:41:47 +0200

    Delete includeCodeFile.js

--- includeCodeFile.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    let includes = $('[data-include]');
-    $.each(includes, function () {
-      let file = 'views/' + $(this).data('include') + '.html'
-      $(this).load(file);
-    })
-})
