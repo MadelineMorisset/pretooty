a5885aaab0ab97dffcf44fdc4bb7b9926579e73d
Madeline MORISSET <madeline.morisset@outlook>
2023-06-14 14:33:03 +0200

    header - réorganisation du repo, ajout d'images, Travail du header

---
+++ views/header.php
@@ -0,0 +1,18 @@
+<header>
+  <nav class="menu">
+    <div class="searchButton">
+      <div class="searchIcon">
+        <i class="fa-solid fa-magnifying-glass fa-2xl"></i>
+        <input id="searchInput" class="searchBar" type="search"  value="Rechercher un outil ...">
+      </div>
+    </div> 
+    
+    <div class="logo">Pretooty</div>
+    
+    <div class="userButton">
+      <div class="userIcon">
+        <i class="fa-solid fa-user fa-2xl"></i>
+      </div>
+    </div> 
+  </nav>
+</header>
