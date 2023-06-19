2b0cb5ebc932576dc81617185ff979cff9b97bd9
Madeline MORISSET <madeline.morisset@outlook>
2023-06-19 09:29:40 +0200

    header - Mise en forme du header

--- views/header.php
+++ views/header.php
@@ -1,18 +1,28 @@
 <header>
-  <nav class="menu">
-    <div class="searchButton">
-      <div class="searchIcon">
-        <i class="fa-solid fa-magnifying-glass fa-2xl"></i>
-        <input id="searchInput" class="searchBar" type="search"  value="Rechercher un outil ...">
+  <div class="navHeader">
+    <div class="search">
+      <div><i class="fa-solid fa-magnifying-glass fa-xl searchButton squareButtons"></i></div>
+      <input id="searchInput" class="searchBar" type="search"  value="Rechercher un outil ...">
+    </div>
+
+    <div class="user">
+      <i class="fa-solid fa-user fa-2x userButton squareButtons"></i>
+
+      <div class="navHidden">
+        <div class="navUserLogin">
+          <a href="loginAccount.php">Me connecter / M’inscrire</a>
+        </div>
+        <div class="navUserAccount">
+          <a href="userPageAccount.php">Mon compte</a>
+          <a href="userPageActivities.php">Mes activités</a>
+          <a href="userMessaging.php">Ma messagerie</a>
+        </div>
+        <div class="navUserLogout">
+          <a href="logoutAccount.php">Me déconnecter</a>
+        </div>
       </div>
     </div> 
-    
-    <div class="logo">Pretooty</div>
-    
-    <div class="userButton">
-      <div class="userIcon">
-        <i class="fa-solid fa-user fa-2xl"></i>
-      </div>
-    </div> 
-  </nav>
+  </div> 
+  
+  <a href="index.php"><div class="logoPretooty"></div></a>
 </header>
