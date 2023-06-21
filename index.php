34f7b3c02f6a16ebf0c3f65e6443d969899a92bb
Madeline MORISSET <madeline.morisset@outlook>
2023-06-21 09:38:16 +0200

    main - Modification des liens CSS et JS (oubliés lors du précédent commit)

--- index.php
+++ index.php
@@ -3,26 +3,24 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Link CSS -->
+        <!-- CSS -->
         <link rel="stylesheet" href="assets/css/style.css">
-        <link rel="stylesheet" href="assets/css/header.css">
-        <link rel="stylesheet" href="assets/css/footer.css">
-        <link rel="stylesheet" href="assets/css/buttons.css">
         <!-- Google Fonts-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Lora&family=Merriweather:wght@700&display=swap" rel="stylesheet"><!-- Titles' Font : "Merriweather" / Texts' Font : "Lora" -->
-        <!-- Link FontAwesome -->
+        <!-- FontAwesome -->
         <script src="https://kit.fontawesome.com/23c1a897ea.js" crossorigin="anonymous"></script>
-        <!-- JS Script -->
-        <script src="assets/js/userButtonHeader.js"></script>
+        <!-- JavaScript -->
+        <script src="assets/js/script.js"></script>
 
         <title>Pretooty</title>
+        <meta name="description" content="">
     </head>
 
     <body>
         <?php include("views/header.php"); ?>
         
-        <!-- <?php include("views/footer.php"); ?> -->
+        <?php include("views/footer.php"); ?>
     </body>
 </html>
